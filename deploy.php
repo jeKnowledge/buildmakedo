2832a23129b3ed52f7e5e05d6efab5c90087423c
Miguel Santos <miguelsantos@fascina.pt>
2018-06-01 12:16:34 +0100

    Fixed title and version

--- deploy.php
+++ deploy.php
@@ -1,10 +1,6 @@
 <?php
-
-// Forked from https://gist.github.com/1809044
-// Available from https://gist.github.com/nichtich/5290675#file-deploy-php
-
-$TITLE   = 'Git Deployment Hamster';
-$VERSION = '0.11';
+$TITLE   = 'Git Deployment';
+$VERSION = '1.0';
 
 echo <<<EOT
 <!DOCTYPE HTML>
